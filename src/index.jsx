969c138a64c71bd48e00c32e235edc4f83ef70b8
MalBourbie <MalBourbie@gmail.com>
2024-09-25 15:30:26 +0200

    progress on home

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Error from "./components/Error"
+import "./styles/main.scss"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import Error from "./components/Error"
 import Home from "./pages/Home"
-import "./styles/main.scss"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,22 @@
-function Home () {
+import "./index.scss"
+import annonces from "./../../annonces.json"
+
+function Home() {
     return (
-        <h1>Page d'acceuil</h1>
+        <main>
+            <section className="main-section">
+                <h1>Chez vous, partout et ailleurs</h1>
+            </section>
+
+            <section className="card-section">
+                {annonces.slice(0, 6).map((annonce) => (
+                    <article key={annonce.id} className="card">
+                        <img src={annonce.cover} alt={annonce.title} className="card__img" />
+                        <h2 className="card__title">{annonce.title}</h2>
+                    </article>
+                ))}
+            </section>
+        </main>
     )
 }
 
