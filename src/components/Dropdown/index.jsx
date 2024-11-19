77d3837bdf555f9ba5c5a1108e1288986821eed0
MalBourbie <MalBourbie@gmail.com>
2024-11-19 19:04:48 +0100

    Dropdown component have a class change according to the page.

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,11 +1,28 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useMemo, useRef, useState } from "react"
 import arrowDropdown from "../../assets/arrow-dropdown.svg"
 import "./index.scss"
+import { useLocation } from "react-router-dom"
 
 function Dropdown({ title, children }) {
     const [isOpen, setIsOpen] = useState(false)
     const [contentHeight, setContentHeight] = useState(0)
     const contentRef = useRef(null)
+    const location = useLocation()
+
+    const pageClass = useMemo(() => {
+        const classesByPage = {
+            "a-propos": "about-page",
+            "logement": "lodging-page",
+        };
+        const pageKey = location.pathname.split("/")[1];
+        const pageClass = classesByPage[pageKey] || "";
+
+        return {
+            dropdown: pageKey === "a-propos" ? "dropdown--about-page" : "",
+            bar: `dropdown__bar--${pageClass}`,
+            dropdownTitle: `dropdown__title--${pageClass}`,
+        };
+    }, [location.pathname]);
 
     useEffect(() => {
         const height = contentRef.current.scrollHeight
@@ -13,15 +30,16 @@ function Dropdown({ title, children }) {
     }, [isOpen])
 
     return (
-        <div className="dropdown">
-            <div className="dropdown__bar">
-                <h2 className="dropdown__title">{title}</h2>
+        <div className={`dropdown ${pageClass.dropdown}`}>
+            <div className={`dropdown__bar ${pageClass.bar}`}>
+                <h2 className={`dropdown__title ${pageClass.dropdownTitle}`}>{title}</h2>
                 <img src={arrowDropdown} alt="déroulement du menu"
                     onClick={() => setIsOpen(!isOpen)}
                     className={`dropdown__arrow ${isOpen ? "dropdown__arrow--active" : ""}`} />
             </div>
             <div className={`dropdown__content ${isOpen ? "" : "dropdown__content--hidden"}`}
-                ref={contentRef}>
+                ref={contentRef}
+            >
                 {children}
             </div>
             {/* add a dynamic space for the content */}
